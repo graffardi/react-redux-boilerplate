a92a8182e34c8acf2b35b14c394143b1693b290b
Unknown <victor.graffard@gmail.com>
2018-04-06 20:39:46 -0400

    app.css, index.jsx, reducers/index.jsx and components/app.jsx added

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../../style/app.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          Whatever you need.
+      </div>
+    );
+  }
+}
